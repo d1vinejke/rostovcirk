68b296c400e04b17d587602174e684bb9cbfa82a
Sergey Kohan <keyn620@gmail.com>
2025-02-26 00:05:52 +0600

    fix css

--- resources/views/detail-afisha.blade.php
+++ resources/views/detail-afisha.blade.php
@@ -57,15 +57,6 @@
                         <address class="not-italic text-gray-600 mt-1">{{ $event->location }}</address>
                     </div>
                 </div>
-
-                <div class="flex items-start space-x-4">
-                    <div class="flex-shrink-0 text-purple-600">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                        </svg>
-                    </div>
-                </div>
-
                 <button class="w-full bg-purple-600 hover:bg-purple-700 text-white py-4 px-6 rounded-lg font-medium transition-colors">
                     <a href="https://rostovcircus.ticketscloud.org/">Купить билет</a>
                 </button>
