e94e21eb419fb306ae06e09c805627c3e70c313c
Sergey Kohan <keyn620@gmail.com>
2025-02-26 12:42:04 +0600

    title change

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Ростовский цирк') }}</title>
-
+    <title>Ростовский цирк</title>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/main.css'])
 </head>
